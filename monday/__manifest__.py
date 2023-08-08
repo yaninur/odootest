1a08451d40cf4f849e2f855764ebe8e5cf099188
yaninur <138690177+yaninur@users.noreply.github.com>
2023-08-08 16:22:15 +0300

    Add files via upload

---
+++ monday/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'ShoesE-commerce',
+    'version': '1.0',
+    'author': 'beyz',
+    'summary': 'filter group by for shoes',
+    'depends': ['product', 'base', 'sale'],
+    'data': [
+        'views/shoes.xml',
+    ],
+    'installable': True,
+    'application': True,
+
+       'demo': [],
+    'images': [
+      'static/description/images',
+      'static/description/images/icon.png',
+ ],
+
+}
